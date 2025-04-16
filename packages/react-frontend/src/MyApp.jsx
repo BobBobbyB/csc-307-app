714d3393b7200510760bdfb16d46fd9efa7c25f2
BobBobbyB <archiephyo@gmail.com>
2025-04-15 20:37:07 -0700

    Connected frontend to backend with GET and POST integration

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,3 +1,4 @@
+// src/MyApp.jsx
 import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
@@ -5,11 +6,25 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  // Fetch all users from backend
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
 
+  // Post a new user to backend
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+
+  // Fetch users once when component mounts
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
@@ -17,13 +32,18 @@ function MyApp() {
       .catch((error) => console.log(error));
   }, []);
 
+  // Submit form: POST to backend, then update state if successful
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => i !== index);
     setCharacters(updated);
-  }
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
   }
 
   return (
